b389b3068064330c3cbbf2181552c8262774739e
Diogo Bozan <diogo_fb_@hotmail.com>
2022-09-26 11:03:17 +0100

    start structure dashboard

---
+++ src/pages/list/List.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./list.scss"
+
+const List = () => {
+    return (
+        <div className='list'>list</div>
+    )
+}
+
+export default List
---
+++ src/pages/new/New.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./new.scss"
+
+const New = () => {
+    return (
+        <div className='new'>new</div>
+    )
+}
+
+export default New;
